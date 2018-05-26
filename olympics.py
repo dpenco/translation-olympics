d3ae77ad4ba46c0b3b730a007d45a5e86614e63b
David Penco <davidpenco@hotmail.com>
2018-05-25 23:28:20 -0700

    Selenium scraping complete
    All the translation web scraping is complete (at least for the set of 100 sentences in each language found in the txt files).

--- olympics.py
+++ olympics.py
@@ -1,12 +1,13 @@
 '''
 Author: David Penco
 Date: 2018-05-24
-TODO: engToChi should clear the textboxes
 '''
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import Select
+import pyperclip
+import time
 
 chrome_options = Options()
 chrome_options.add_argument('--disable-infobars')
@@ -14,11 +15,20 @@ chrome_options.add_experimental_option('prefs', \
 {'intl.accept_languages': 'en-US'})
 driver = webdriver.Chrome(chrome_options=chrome_options)
 
+courtesyDelay = 5
+
 sites = {'baidu': 'https://fanyi.baidu.com/', \
 'bing': 'https://www.bing.com/translator', \
 'google': 'https://translate.google.com/', \
 'sogou': 'https://fanyi.sogou.com', \
 'yandex': 'https://translate.yandex.com/'}
+
+inputBox = {'baidu': 'baidu_translate_input', 'bing': 't_sv', \
+'google': 'source', 'sogou': 'sogou-translate-input', 'yandex': 'fakeArea'}
+
+output = {}
+for site in sites:
+    output[site] = []
 
 tabOrder = {}
 tabCounter = 0
@@ -44,77 +54,107 @@ def engToChi(site):
         driver.find_element_by_xpath('//a[@lang="zh-CHS"][@data-type="to"]').click()
     elif site == 'yandex':
         driver.find_element_by_id('dstLangButton').click()
+        time.sleep(0.5)
         driver.find_element_by_xpath('(//div[@data-value="zh"])[2]').click()
         driver.find_element_by_id('srcLangButton').click()
+        time.sleep(0.5)
         driver.find_element_by_xpath('(//div[@data-value="en"])').click()
 
+def chiToEng(site):
+    if site == 'baidu':
+        driver.find_element_by_class_name('select-from-language').click()
+        driver.find_element_by_xpath('//a[@value="zh"]').click()
+        driver.find_element_by_class_name('select-to-language').click()
+        driver.find_element_by_xpath('(//a[@value="en"])[3]').click()
+    elif site == 'bing':
+        Select(driver.find_element_by_id('t_sl')).select_by_visible_text('Chinese Simplified')
+        Select(driver.find_element_by_id('t_tl')).select_by_visible_text('English')
+    elif site == 'google':
+        driver.find_element_by_id('gt-sl-gms').click()
+        driver.find_element_by_id(':g').click()
+        driver.find_element_by_id('gt-tl-gms').click()
+        driver.find_element_by_id(':3j').click()
+    elif site == 'sogou':
+        driver.find_element_by_xpath('//em[@data-langtype="from"]').click()
+        driver.find_element_by_xpath('//a[@lang="zh-CHS"]').click()
+        driver.find_element_by_xpath('//em[@data-langtype="to"]').click()
+        driver.find_element_by_xpath('//a[@lang="en"][@data-type="to"]').click()
+    elif site == 'yandex':
+        driver.find_element_by_id('dstLangButton').click()
+        time.sleep(0.5)
+        driver.find_element_by_xpath('(//div[@data-value="en"])[2]').click()
+        driver.find_element_by_id('srcLangButton').click()
+        time.sleep(0.5)
+        driver.find_element_by_xpath('(//div[@data-value="zh"])').click()
 
-for name, url in sites.items():
-    tabOrder[name] = tabCounter
-    tabCounter += 1
-    driver.get(url)
-    engToChi(name)
-    if len(driver.window_handles) < len(sites):
-        driver.execute_script('window.open("about:blank");')
-        driver.switch_to.window(driver.window_handles[-1])
+def enterInput(site, sentence):
+    driver.find_element_by_id(inputBox[site]).clear()
+    driver.find_element_by_id(inputBox[site]).send_keys(sentence)
 
+def getOutput(site):
+    if site == 'baidu':
+        return driver.find_element_by_class_name('target-output').text
+    elif site == 'bing':
+        driver.find_element_by_id('t_copyIcon').click()
+        return pyperclip.paste()
+    elif site == 'google':
+        return driver.find_element_by_id('result_box').text
+    elif site == 'sogou':
+        return driver.find_element_by_id('sogou-translate-output').text
+    elif site == 'yandex':
+        return driver.find_element_by_id('translation').text
+    else:
+        return ''
 
+def openWebsites(tabCounter, fromEnglish=True):
+    for name in sites:
+        tabOrder[tabCounter] = name
+        tabCounter += 1
+        driver.get(sites[name])
+        if fromEnglish:
+            engToChi(name)
+        else:
+            chiToEng(name)
+        if len(driver.window_handles) < len(sites):
+            driver.execute_script('window.open("about:blank");')
+            driver.switch_to.window(driver.window_handles[-1])
 
-input()
-driver.quit()
+def engToChiWorkflow(tabCounter):
+    openWebsites(tabCounter, fromEnglish=True)
+    with open('newEnglish.txt', 'r') as englishInput:
+        for sentence in englishInput:
+            if sentence != '':
+                for tab in tabOrder:
+                    driver.switch_to.window(driver.window_handles[tab])
+                    enterInput(tabOrder[tab], sentence)
+                time.sleep(courtesyDelay)
+                for tab in tabOrder:
+                    driver.switch_to.window(driver.window_handles[tab])
+                    output[tabOrder[tab]].append(getOutput(tabOrder[tab]))
+    driver.quit()
+    with open('newChineseOutput.txt', 'w') as chineseOutput:
+        for site in output:
+            chineseOutput.write('%s\n' % site)
+            for sentence in output[site]:
+                chineseOutput.write('%s\n' % sentence)
 
-'''
-with open('chinese.txt','r') as en:
-    with open('zhen.txt','a') as zh:
-        for sentence in en:
-            # 搜狗翻译
-            k.press_keys(['Control_L', 'Tab'])
-            time.sleep(delay)
-            m.click(355, 300, 1, 3)
-            time.sleep(delay)
-            pyperclip.copy(sentence.replace('\ufeff', '').strip())
-            time.sleep(delay)
-            k.press_keys(['Control_L', 'v'])
-            time.sleep(delay)
-            m.click(960, 300, 1, 3)
-            time.sleep(delay)
-            k.press_keys(['Control_L', 'c'])
-            time.sleep(delay)
-            translation = pyperclip.paste()
-            zh.write(translation)
-            time.sleep(delay)
-            # 百度翻译
-            k.press_keys(['Control_L', 'Tab'])
-            time.sleep(delay)
-            m.click(357, 238, 1, 3)
-            time.sleep(delay)
-            pyperclip.copy(sentence.replace('\ufeff', '').strip())
-            time.sleep(delay)
-            k.press_keys(['Control_L', 'v'])
-            time.sleep(delay)
-            m.click(850, 200)
-            time.sleep(delay)
-            m.click(962, 237, 1, 3)
-            time.sleep(delay)
-            k.press_keys(['Control_L', 'c'])
-            time.sleep(delay)
-            translation = pyperclip.paste()
-            zh.write(translation)
-            time.sleep(delay)
-            # 谷歌翻译
-            k.press_keys(['Control_L', 'Tab'])
-            time.sleep(delay)
-            m.click(37, 285, 1, 3)
-            time.sleep(delay)
-            pyperclip.copy(sentence.replace('\ufeff', '').strip())
-            time.sleep(delay)
-            k.press_keys(['Control_L', 'v'])
-            time.sleep(delay)
-            m.click(680, 285, 1, 3)
-            time.sleep(delay)
-            k.press_keys(['Control_L', 'c'])
-            time.sleep(delay)
-            translation = pyperclip.paste()
-            zh.write(translation)
-            time.sleep(delay)
-'''
+def chiToEngWorkflow(tabCounter):
+    openWebsites(tabCounter, fromEnglish=False)
+    with open('newChinese.txt', 'r') as chineseInput:
+        for sentence in chineseInput:
+            if sentence != '':
+                for tab in tabOrder:
+                    driver.switch_to.window(driver.window_handles[tab])
+                    enterInput(tabOrder[tab], sentence)
+                time.sleep(courtesyDelay)
+                for tab in tabOrder:
+                    driver.switch_to.window(driver.window_handles[tab])
+                    output[tabOrder[tab]].append(getOutput(tabOrder[tab]))
+    driver.quit()
+    with open('newEnglishOutput.txt', 'w') as englishOutput:
+        for site in output:
+            englishOutput.write('%s\n' % site)
+            for sentence in output[site]:
+                englishOutput.write('%s\n' % sentence)
+
+chiToEngWorkflow(tabCounter)
