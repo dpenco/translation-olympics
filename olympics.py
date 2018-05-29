86c5bf891612034502ea7b305d86f8f740a58fe4
David Penco <davidpenco@hotmail.com>
2018-05-28 22:43:05 -0700

    making classes and methods
    halfway through converting the code to proper object-oriented classes with methods, to make it easier to use other datasets later.
    
    this version will not compile, but the next one will. stay tuned

--- olympics.py
+++ olympics.py
@@ -10,82 +10,121 @@ from selenium.webdriver.support.ui import Select
 import pyperclip
 import time
 
-chrome_options = Options()
-chrome_options.add_argument('--disable-infobars')
-chrome_options.add_experimental_option('prefs', \
-{'intl.accept_languages': 'en-US'})
-driver = webdriver.Chrome(chrome_options=chrome_options)
+'''
+Chrome Webdriver, based on Selenium
+@Param driver: the Selenium webdriver object
+@Param courtesyDelay: time delay, in seconds, to respect terms of service
+@Param dictionaries: all the dictionaries used to drive the webscraping
+'''
+class ChromeDriver:
+    def __init__(self):
+        chrome_options = Options()
+        chrome_options.add_argument('--disable-infobars')
+        chrome_options.add_experimental_option('prefs', \
+        {'intl.accept_languages': 'en-US'})
+        self.driver = webdriver.Chrome(chrome_options=chrome_options)
+        self.courtesyDelay = 8
+        self.dictionary = WebsiteDictionaries()
+        setupChiToEng(self)
 
-courtesyDelay = 8
+    def setupChiToEng(self):
+        tabNumber = 0
+        self.driver.switch_to.window(self.driver.window_handles[0])
+        for site in self.dictionary.url:
+            self.driver.get(self.dictionary.url[site])
+            if site.capitalize() == 'Baidu':
+                self.driver.find_element_by_class_name('select-from-language').click()
+                self.driver.find_element_by_xpath('//a[@value="zh"]').click()
+                self.driver.find_element_by_class_name('select-to-language').click()
+                self.driver.find_element_by_xpath('(//a[@value="en"])[3]').click()
+            elif site.capitalize() == 'Bing':
+                Select(self.driver.find_element_by_id('t_sl')).select_by_visible_text('Chinese Simplified')
+                Select(self.driver.find_element_by_id('t_tl')).select_by_visible_text('English')
+            elif site.capitalize() == 'Google':
+                self.driver.find_element_by_id('gt-sl-gms').click()
+                self.driver.find_element_by_id(':g').click()
+                self.driver.find_element_by_id('gt-tl-gms').click()
+                self.driver.find_element_by_id(':3j').click()
+            elif site.capitalize() == 'Sogou':
+                self.driver.find_element_by_xpath('//em[@data-langtype="from"]').click()
+                self.driver.find_element_by_xpath('//a[@lang="zh-CHS"]').click()
+                self.driver.find_element_by_xpath('//em[@data-langtype="to"]').click()
+                self.driver.find_element_by_xpath('//a[@lang="en"][@data-type="to"]').click()
+            elif site.capitalize() == 'Yandex':
+                self.driver.find_element_by_id('dstLangButton').click()
+                time.sleep(0.5)
+                self.driver.find_element_by_xpath('(//div[@data-value="en"])[2]').click()
+                self.driver.find_element_by_id('srcLangButton').click()
+                time.sleep(0.5)
+                self.driver.find_element_by_xpath('(//div[@data-value="zh"])').click()
+            self.dictionary.tabOrder[tabNumber] = site
+            if len(self.driver.window_handles) < len(self.dictionary.url):
+                self.driver.execute_script('window.open(\'about:blank\');')
+                self.driver.switch_to.window(self.driver.window_handles[-1])
+                tabNumber += 1
+        self.driver.switch_to.window(self.driver.window_handles[0])
 
-sites = {'baidu': 'https://fanyi.baidu.com/', \
-'bing': 'https://www.bing.com/translator', \
-'google': 'https://translate.google.com/', \
-'sogou': 'https://fanyi.sogou.com', \
-'yandex': 'https://translate.yandex.com/'}
+    def setupEngToChi(self):
+        tabNumber = 0
+        self.driver.switch_to.window(self.driver.window_handles[0])
+        for site in self.dictionary.url:
+            self.driver.get(self.dictionary.url[site])
+            if site.capitalize() == 'Baidu':
+                self.driver.find_element_by_class_name('select-from-language').click()
+                self.driver.find_element_by_xpath('//a[@value="en"]').click()
+                self.driver.find_element_by_class_name('select-to-language').click()
+                self.driver.find_element_by_xpath('(//a[@value="zh"])[3]').click()
+            elif site.capitalize() == 'Bing':
+                Select(self.driver.find_element_by_id('t_sl')).select_by_visible_text('English')
+                Select(self.driver.find_element_by_id('t_tl')).select_by_visible_text('Chinese Simplified')
+            elif site.capitalize() == 'Google':
+                self.driver.find_element_by_id('gt-sl-gms').click()
+                self.driver.find_element_by_id(':m').click()
+                self.driver.find_element_by_id('gt-tl-gms').click()
+                self.driver.find_element_by_id(':3c').click()
+            elif site.capitalize() == 'Sogou':
+                self.driver.find_element_by_xpath('//em[@data-langtype="from"]').click()
+                self.driver.find_element_by_xpath('//a[@lang="en"]').click()
+                self.driver.find_element_by_xpath('//em[@data-langtype="to"]').click()
+                self.driver.find_element_by_xpath('//a[@lang="zh-CHS"][@data-type="to"]').click()
+            elif site.capitalize() == 'Yandex':
+                self.driver.find_element_by_id('dstLangButton').click()
+                time.sleep(0.5)
+                self.driver.find_element_by_xpath('(//div[@data-value="zh"])[2]').click()
+                self.driver.find_element_by_id('srcLangButton').click()
+                time.sleep(0.5)
+                self.driver.find_element_by_xpath('(//div[@data-value="en"])').click()
+            self.dictionary.tabOrder[tabNumber] = site
+            if len(self.driver.window_handles) < len(self.dictionary.url):
+                self.driver.execute_script('window.open(\'about:blank\');')
+                self.driver.switch_to.window(self.driver.window_handles[-1])
+                tabNumber += 1
+        self.driver.switch_to.window(self.driver.window_handles[0])
 
-inputBox = {'baidu': 'baidu_translate_input', 'bing': 't_sv', \
-'google': 'source', 'sogou': 'sogou-translate-input', 'yandex': 'fakeArea'}
+'''
+Dictionaries used to make the code body more modular and easier to edit
+@Param url: maps website names to their url
+@Param inputBox: maps website names to the id of their translation input box
+@Param output: maps website names to their translation output
+@Param tabOrder: maps webdriver tab indices to site names
+'''
+class WebsiteDictionaries:
+    def __init__(self):
+        self.url = {'Baidu': 'https://fanyi.baidu.com/', \
+        'Bing': 'https://www.bing.com/translator', \
+        'Google': 'https://translate.google.com/', \
+        'Sogou': 'https://fanyi.sogou.com', \
+        'Yandex': 'https://translate.yandex.com/'}
 
-output = {}
-for site in sites:
-    output[site] = []
+        self.inputBox = {'Baidu': 'baidu_translate_input', 'Bing': 't_sv', \
+        'Google': 'source', 'Sogou': 'sogou-translate-input', 'Yandex': \
+        'fakeArea'}
 
-tabOrder = {}
+        self.output = {}
+        for site in self.url:
+            self.output[site] = []
 
-def engToChi(site):
-    if site == 'baidu':
-        driver.find_element_by_class_name('select-from-language').click()
-        driver.find_element_by_xpath('//a[@value="en"]').click()
-        driver.find_element_by_class_name('select-to-language').click()
-        driver.find_element_by_xpath('(//a[@value="zh"])[3]').click()
-    elif site == 'bing':
-        Select(driver.find_element_by_id('t_sl')).select_by_visible_text('English')
-        Select(driver.find_element_by_id('t_tl')).select_by_visible_text('Chinese Simplified')
-    elif site == 'google':
-        driver.find_element_by_id('gt-sl-gms').click()
-        driver.find_element_by_id(':m').click()
-        driver.find_element_by_id('gt-tl-gms').click()
-        driver.find_element_by_id(':3c').click()
-    elif site == 'sogou':
-        driver.find_element_by_xpath('//em[@data-langtype="from"]').click()
-        driver.find_element_by_xpath('//a[@lang="en"]').click()
-        driver.find_element_by_xpath('//em[@data-langtype="to"]').click()
-        driver.find_element_by_xpath('//a[@lang="zh-CHS"][@data-type="to"]').click()
-    elif site == 'yandex':
-        driver.find_element_by_id('dstLangButton').click()
-        time.sleep(0.5)
-        driver.find_element_by_xpath('(//div[@data-value="zh"])[2]').click()
-        driver.find_element_by_id('srcLangButton').click()
-        time.sleep(0.5)
-        driver.find_element_by_xpath('(//div[@data-value="en"])').click()
-
-def chiToEng(site):
-    if site == 'baidu':
-        driver.find_element_by_class_name('select-from-language').click()
-        driver.find_element_by_xpath('//a[@value="zh"]').click()
-        driver.find_element_by_class_name('select-to-language').click()
-        driver.find_element_by_xpath('(//a[@value="en"])[3]').click()
-    elif site == 'bing':
-        Select(driver.find_element_by_id('t_sl')).select_by_visible_text('Chinese Simplified')
-        Select(driver.find_element_by_id('t_tl')).select_by_visible_text('English')
-    elif site == 'google':
-        driver.find_element_by_id('gt-sl-gms').click()
-        driver.find_element_by_id(':g').click()
-        driver.find_element_by_id('gt-tl-gms').click()
-        driver.find_element_by_id(':3j').click()
-    elif site == 'sogou':
-        driver.find_element_by_xpath('//em[@data-langtype="from"]').click()
-        driver.find_element_by_xpath('//a[@lang="zh-CHS"]').click()
-        driver.find_element_by_xpath('//em[@data-langtype="to"]').click()
-        driver.find_element_by_xpath('//a[@lang="en"][@data-type="to"]').click()
-    elif site == 'yandex':
-        driver.find_element_by_id('dstLangButton').click()
-        time.sleep(0.5)
-        driver.find_element_by_xpath('(//div[@data-value="en"])[2]').click()
-        driver.find_element_by_id('srcLangButton').click()
-        time.sleep(0.5)
-        driver.find_element_by_xpath('(//div[@data-value="zh"])').click()
+        self.tabOrder = {}
 
 def enterInput(site, sentence):
     driver.find_element_by_id(inputBox[site]).clear()
@@ -105,19 +144,6 @@ def getOutput(site):
         return driver.find_element_by_id('translation').text
     else:
         return ''
-
-def openWebsites(tabCounter, fromEnglish=True):
-    for name in sites:
-        tabOrder[tabCounter] = name
-        tabCounter += 1
-        driver.get(sites[name])
-        if fromEnglish:
-            engToChi(name)
-        else:
-            chiToEng(name)
-        if len(driver.window_handles) < len(sites):
-            driver.execute_script('window.open("about:blank");')
-            driver.switch_to.window(driver.window_handles[-1])
 
 def engToChiWorkflow(tabCounter):
     openWebsites(tabCounter, fromEnglish=True)
