47efb4bc8c34fed3de3c1b57597710136ca79a96
David Penco <davidpenco@hotmail.com>
2018-05-30 22:13:12 -0700

    Delete seg.py

--- seg.py
+++
@@ -1,6 +0,0 @@
-import jieba
-
-with open('newChineseOutputPostProcessed.txt','r') as unsegmented: # newChineseOutputPostProcessed newChineseDesegmented
-    with open('newChineseOutputPostProcessedSegmented.txt','w') as segmented: #newChineseOutputPostProcessedSegmented newChineseDeAndReSegmented
-        for sentence in unsegmented:
-            segmented.write(' '.join(jieba.cut(sentence)))
